ea4dda35b9c0cc58915f8193e2576b7dd93fab61
sashkkkkk <s4ny4@yandex.ru>
2023-02-06 16:29:04 +0300

    Tests added

---
+++ src/main/java/ru/gb/lesson4/Test.java
@@ -0,0 +1,46 @@
+package ru.gb.lesson4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Test {public static void main(String[] args) {
+    meth();
+}
+    public static void meth() {
+        Random rand = new Random();
+        int[] arr = new int[10];
+        for(int i = 0; i < arr.length; i++){
+            int x = rand.nextInt(100);
+            arr[i] = x;
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Max value is " + max);
+
+        int min = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Min value is " + min);
+
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println("Summ of numbers is " + sum);;
+    }
+
+
+
+
+
+}
