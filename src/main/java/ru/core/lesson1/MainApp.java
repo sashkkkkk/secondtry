fed6ce8e500587cc4a35b38bd7bf61ca07474e2a
sashkkkkk <s4ny4@yandex.ru>
2023-02-18 15:10:26 +0300

    filter by age created

---
+++ src/main/java/ru/core/lesson1/MainApp.java
@@ -0,0 +1,13 @@
+package ru.core.lesson1;
+
+import java.util.Arrays;
+
+public class MainApp {
+    public static void main(String[] args) {
+      Team team1 = new Team("ЖУжа");
+      team1.getInfo();
+      Course c = new Course();
+      System.out.println(c);
+
+    }
+}
