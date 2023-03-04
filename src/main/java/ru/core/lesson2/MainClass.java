0df067dcf17ed651f19a4c038f9c55341b3e9e81
sashkkkkk <s4ny4@yandex.ru>
2023-03-04 11:54:17 +0300

    Exeption continued

--- src/main/java/ru/core/lesson2/MainClass.java
+++ src/main/java/ru/core/lesson2/MainClass.java
@@ -1,41 +1,25 @@
 package ru.core.lesson2;
 
 public class MainClass {
-   /* public static void main(String[] args) {
-        try {
-            int a = 10;
-            a -= 10;
-            int b = 42 / a;
-            int[] с = {1, 2, 3};
-            с[42] = 99;
-        } catch (ArithmeticException e) {
-            System.out.println("Дeлeниe на ноль: " + e);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Ошибка индексации массива: " + e);
-        }
-        System.out.println("Пocлe блока операторов try/catch");
-    }*/
-
     public static void main(String[] args) {
         methodArray();
+
     }
 
-    public static void methodArray() {
-        int[][] arr = new int[4][4];
-        try {
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                    arr[i][j] = 1;
-                    System.out.print(arr[i][j] + " ");
-                }
-                System.out.println();
-                throw new ArrayIndexOutOfBoundsException();
-            }
-            }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Array index out of bounds");
-
+    public static void methodArray() throws MyArraySizeException {
+        String[][] arr = new String[4][4];
+        if(arr.length > 4){
+            throw new  MyArraySizeException("Превышение длины массива");
         }
-    }
 
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length; j++){
+                arr[i][j] = "aaa";
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
 
---
+++ src/main/java/ru/core/lesson2/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package ru.core.lesson2;
+
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+}
